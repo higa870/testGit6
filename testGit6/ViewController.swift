b673b531748fd7936baf23ed01277660f3fceaa9
higa870 <htarou870@gmail.com>
2024-06-15 18:28:15 +0900

    sixth commit

--- testGit6/ViewController.swift
+++ testGit6/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func button(_ sender: Any) {
+        label.text = "Hello!!"
+    }
+    
 
 }
 
